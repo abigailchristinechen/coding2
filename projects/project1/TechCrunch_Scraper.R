1caf6feba576aec579004200c63092f5e4d0f907
abigailchristinechen <abigailchristinechen@gmail.com>
2022-01-06 09:50:10 +0100

    added fixed TechCrunch Scraper File

---
+++ projects/project1/TechCrunch_Scraper.R
@@ -0,0 +1,15 @@
+##Coding 2: News Scraper
+##TECHCRUNCH SCRAPER
+##Abigail Chen
+
+## libraries used for data handling
+library(tidyverse)
+library(rvest)
+library(stringr)
+library(xml2)
+library(httr)  
+library(tidyr) 
+library(reshape2)
+library(RCurl)
+library(htmltidy)
+library(XML)
