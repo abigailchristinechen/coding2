89f962b9466eb018dd604d43bf51511b37f2b385
abigailchristinechen <abigailchristinechen@gmail.com>
2022-01-06 11:58:30 +0100

    loading the libraries

---
+++ projects/project2/Twitter_API.R
@@ -0,0 +1,17 @@
+##Coding 2: Twitter API
+##Topic: Axie Infinity
+##Tags: "Axie", "Axie Infinity" , "AXS", "SLP", "Manager", "Scholar", "Scholarship", "Sky Mavis" 
+##Abigail Chen
+
+#Load Libraries
+library("base64enc")
+library("openssl")
+library("httpuv")
+library("twitteR")
+library("tm")
+library("stringr")
+library("dplyr")
+library("httr")
+library("tidyverse")
+library("tidytext")
+library('plotly')
